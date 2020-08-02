494b1d7df27e91850895a9e5f8a7e4b1b093564f
askr-ssy <Askr-ssy@users.noreply.github.com>
2020-08-02 13:06:38 +0800

    1.0

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ import sys
 import socket
 import binascii
 
-# magi_packet = binascii.hexlify("FF"*6,str(mac)*16)
-# row = socket.socket(socket.PF_PACKET,socket.SOCK_RAW,socket.htonl(0x1234))
-# row.bind(("wlp3s0",0))
-
 def format_packet(mac):
     def to_hex_int(s):
         return int(s.upper(), 16)
@@ -28,12 +24,12 @@ def format_packet(mac):
     elif __==5 and _==0:
         mac = mac.split("-",5)
 
-    return chr(255)*6 + "".join([chr(int(ch,16)) for ch in mac]*16) + chr(0) *6
+    return binascii.unhexlify('FF'*6+"".join(mac*16))
 
 def send_packet(packet):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
-    s.sendto(packet.encode(),('255.255.255.255'.encode(),9))
+    s.sendto(packet,('255.255.255.255',9))
 
 
 if __name__ == "__main__":
@@ -43,3 +39,4 @@ if __name__ == "__main__":
     send_packet(format_packet(sys.argv[1]))
     # wake on lan 04:D4:C4:F0:71:38
     # wake on lan 9C:B6:54:A9:A3:11
+    # wake on lan 68:F7:28:18:64:DC
